b077f2e107c7b8b51d1564d8b921107614b05196
aqsurine <88474556+yppeu@users.noreply.github.com>
2022-07-11 23:36:28 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function show(){
+    document.getElementById('disabled').style.display = 'block';
+    button.style.display = none;
+}
+
+function hide(){
+    document.getElementsByClassName('sub2').innerText = '그래도 한 번 보고 가실게요~;'
+}
